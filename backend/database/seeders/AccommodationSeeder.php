d1aee445eb01f1cac5b138ee2c329f8cf4b4db71
OfilioHA <herdocia20@gmail.com>
2024-06-28 17:52:16 -0600

    feat: :sparkles: Seeding all models (Accommodation, locations and details)

--- backend/database/seeders/AccommodationSeeder.php
+++ backend/database/seeders/AccommodationSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Accommodation;
+use App\Models\AccommodationDetails;
+use App\Models\AccommodationLocation;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,9 +21,18 @@ class AccommodationSeeder extends Seeder
         foreach ($fileContent as $index => $line) {
             if (!$index) continue;
             $row = str_getcsv($line);
+            // Save Accommodation
             $accommodation = self::formatAccommodation($row);
             $accommodation = new Accommodation($accommodation);
             $accommodation->save();
+            // Save Location
+            $location = self::formatLocation($row);
+            $location = new AccommodationLocation($location);
+            $accommodation->location()->save($location);
+            // Save Details
+            $details = self::formatDetails($row);
+            $details = new AccommodationDetails($details);
+            $accommodation->location()->save($details);
         }
     }
 
@@ -47,5 +58,59 @@ class AccommodationSeeder extends Seeder
             'register_at' => $date,
             'useful_meters' => (int) $row[39],
         ];
+    }
+
+    public static function formatLocation($row)
+    {
+        return [
+            'latitude' => $row[0],
+            'longitude' => $row[1],
+            'address' => $row[11],
+            'province' => $row[12],
+            'city' => $row[13],
+            'street' => $row[29],
+            'neighborhood' => $row[30],
+            'district' => $row[31],
+        ];
+    }
+
+    public static function formatDetails($row)
+    {
+        $heating = ($row[22] == 'TRUE') ? 'SI' : $row[22];
+
+        return [
+            // Variables Data
+            'individual_heating' => $heating,
+            'energetic_certification' => $row[23],
+            // Numeric Data
+            'plants' => (int) $row[41],
+            'bedrooms' => (int) $row[15],
+            'bathrooms' => (int) $row[16],
+            'floor' => (int) $row[24],
+            // Boolean Data
+            'reformed' => self::formatBoolean($row[6]),
+            'parking' => self::formatBoolean($row[17]),
+            'second_hand' => self::formatBoolean($row[18]),
+            'built_in_wardrobes' => self::formatBoolean($row[19]),
+            'furnished' => self::formatBoolean($row[21]),
+            'exterior' => self::formatBoolean($row[25]),
+            'interior' => self::formatBoolean($row[26]),
+            'elevator' => self::formatBoolean($row[27]),
+            'terrace' => self::formatBoolean($row[32]),
+            'storage_room' => self::formatBoolean($row[33]),
+            'equipped_kitchen' => self::formatBoolean($row[34]),
+            'air_conditioning' => self::formatBoolean($row[36]),
+            'pool' => self::formatBoolean($row[37]),
+            'garden' => self::formatBoolean($row[38]),
+            'balcony' => self::formatBoolean($row[43]),
+            'suitable_mobility' => self::formatBoolean($row[40]),
+            'pets_allowed' => self::formatBoolean($row[42]),
+        ];
+    }
+
+    public static function formatBoolean(string $value)
+    {
+        $value = strtolower($value);
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
     }
 }
