8bf928e6616fa3588dec2be881d50c09c8d6c7ad
OfilioHA <herdocia20@gmail.com>
2024-06-28 17:48:52 -0600

    test: :white_check_mark: Testing if all records have same amount

--- backend/tests/Feature/AccommodationTest.php
+++ backend/tests/Feature/AccommodationTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\Accommodation;
+use App\Models\AccommodationDetails;
+use App\Models\AccommodationLocation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,5 +20,17 @@ class AccommodationTest extends TestCase
     {
         $amount = Accommodation::count();
         $this->assertEquals(self::EXPECTED_AMOUNT, $amount);
+    }
+
+    /**
+     * Testing the amount equal with the csv.
+     */
+    public function test_accommodations_amount_location_details(): void
+    {
+        $accommodation = Accommodation::count();
+        $location = AccommodationLocation::count();
+        $details = AccommodationDetails::count();
+        $equals = ($accommodation == $location &&  $accommodation == $details);
+        $this->assertEquals($equals, true);
     }
 }
