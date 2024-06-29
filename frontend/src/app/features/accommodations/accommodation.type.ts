1fd20661027c6f803d5e7d7997ef642953bd1867
OfilioHA <herdocia20@gmail.com>
2024-06-29 13:01:52 -0600

    feat: :children_crossing: Improving item data

--- frontend/src/app/features/accommodations/accommodation.type.ts
+++ frontend/src/app/features/accommodations/accommodation.type.ts
@@ -4,8 +4,10 @@ export interface IAccomodationsItem {
     id: number;
     title: string;
     description: string;
-    register_at: string;
     advertiser: string;
+    register_at?: string;
+    price: number;
+    meter_price: number;
 }
 
 
